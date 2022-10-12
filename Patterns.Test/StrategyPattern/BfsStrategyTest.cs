0485c4f60b950dfbec7fd5cd9a9a801a639a3ee6
Ehsan <e.toghian@gmail.com>
2022-10-12 15:32:29 +0330

    Add tests

---
+++ Patterns.Test/StrategyPattern/BfsStrategyTest.cs
@@ -0,0 +1,61 @@
+﻿using Strategy.MidLevel_Example;
+
+namespace Patterns.Test.StrategyPattern
+{
+    public class BfsStrategyTest
+    {
+        private readonly Dictionary<int, int[]> _adjacencySimulator;
+
+        // The graph
+        // ┌────┐      ┌────┐
+        // │ 15 │      │ 17 │
+        // │    ├─────►│    │
+        // └──┬─┘      └────┘
+        //    │        ┌────┐       ┌────┐     ┌────┐
+        //    │        │ 5  │       │ 4  │     │ 8  │
+        //    └───────►│    ├──────►│    ├────►│    │
+        //             └────┘       └──┬─┘     └────┘
+        //                             │       ┌────┐
+        //                             │       │ 9  │
+        //                             └──────►│    │
+        //                                     └────┘
+
+        public BfsStrategyTest()
+        {
+            // Although declaring a data structure for the graph is more readable,
+            // It is sufficient here to simulate a graph with a dictionary. 
+            _adjacencySimulator = new Dictionary<int, int[]>
+            {
+                // Make the graph by forming the relations between nodes
+                { 15, new[] { 17, 5 } },
+                { 5,  new[] { 4 } },
+                { 4,  new[] {8, 9} }
+            };
+        }
+
+        [Fact]
+        public void BfsShouldMeetAllNodes()
+        {
+            int root = 15;
+
+            // This function simply returns the adjacncy list of a vertex
+            Func<int, ICollection<int>> adjacencyCalculator = (node) =>
+                _adjacencySimulator.TryGetValue(node, out int[]? adjacencyList)
+                    ? (int[])adjacencyList
+                    : new List<int>();
+
+            var bfsSearch = new Bfs<int>(adjacencyCalculator);
+            var algorithm = new GraphTraverse<int>(bfsSearch);
+            var nodes = algorithm.GetNodes(root);
+
+            Assert.NotNull(nodes);
+            Assert.NotEmpty(nodes);
+
+            // Sum of all nodes
+            var expected = 15 + (5 + 17) + 4 + (8 + 9);
+            var actual = nodes.Sum();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Patterns.Test/StrategyPattern/DfsStrategyTest.cs
@@ -0,0 +1,54 @@
+﻿using Strategy.MidLevel_Example;
+using Strategy.MidLevel_Example.Models;
+
+namespace Patterns.Test.StrategyPattern
+{
+    public class DfsStrategyTest
+    {
+        private readonly Graph<int> _graph; 
+
+        // The graph
+        // ┌────┐      ┌────┐
+        // │ 15 │      │ 17 │
+        // │    ├─────►│    │
+        // └──┬─┘      └────┘
+        //    │        ┌────┐       ┌────┐     ┌────┐
+        //    │        │ 5  │       │ 4  │     │ 8  │
+        //    └───────►│    ├──────►│    ├────►│    │
+        //             └────┘       └──┬─┘     └────┘
+        //                             │       ┌────┐
+        //                             │       │ 9  │
+        //                             └──────►│    │
+        //                                     └────┘
+
+        public DfsStrategyTest()
+        {
+            _graph = new Graph<int>(
+                vertices: new List<int>() { 15, 17, 5, 4, 8, 9 },
+                edges: new List<Tuple<int, int>>()
+                {
+                    Tuple.Create(15, 17), Tuple.Create(15, 5),
+                    Tuple.Create(5, 4),
+                    Tuple.Create(4, 8), Tuple.Create(4, 9)
+                });
+        }
+
+        [Fact]
+        public void DfsShouldMeetAllNodes()
+        {
+            ISearchStrategy<int> dfsSearch = new Dfs<int>(_graph);
+            GraphTraverse<int>   algorithmRunner = new(dfsSearch);
+
+            int rootElement = 15;
+            var nodes = algorithmRunner.GetNodes(rootElement);
+
+            Assert.True(nodes.Count > 0);
+
+            // Expect to meet all nodes
+            var expected = _graph.Vertices.Sum();
+            var actual = nodes.Sum();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
